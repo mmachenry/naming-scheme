20f177ffe51b76831f77ee767b76a281a0e83bd1
Mike MacHenry <mike.machenry@gmail.com>
2018-07-14 20:28:46 -0400

    Remove old todo comment

--- src/Compiler.elm
+++ src/Compiler.elm
@@ -3,13 +3,6 @@ module Compiler exposing (compileToClass)
 import DeBruijn exposing (..)
 import String
 
--- TODO need to insert parenthesis minimally. See TAPL
--- Currently no parens are inserter at all and thus this is incorrect ATM.
-
--- TODO the encoding of let is wrong. We only need let,in,end, not = and I'm
--- not sure which Randy wants because his current code has Service for both
--- = and and end.
-
 compileToClass : BTerm -> String
 compileToClass term = String.join "" (List.map alphaConvert (pprBTerm term))
 
